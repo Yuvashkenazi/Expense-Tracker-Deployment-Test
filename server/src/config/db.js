308fb2a468d7a615e53d1fdd09a314d74aa71c54
Yuval Ashkenazi <ashkenaziyuval@gmail.com>
2025-11-23 15:00:04 -0600

    * removed unused mysql code (using prisma instead)

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,15 +3,12 @@ const { PrismaClient } = require('@prisma/client');
 const { PrismaPg } = require('@prisma/adapter-pg');
 require('dotenv').config();
 
-// Create a PostgreSQL connection pool
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-// Create Prisma adapter with pg pool
 const adapter = new PrismaPg(pool);
 
-// Create Prisma client with the adapter
 const prisma = new PrismaClient({ adapter });
 
 const testConnection = async (callback) => {
